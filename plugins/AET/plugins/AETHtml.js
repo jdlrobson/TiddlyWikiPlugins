40704d062760bf2399f12b711593521831c834f0
Jon Robson <jdlrobson@gmail.com>
2011-01-28 15:58:40 +0000

    AETHtml: prevent false being saved to field
    only add html if value exists

--- plugins/AET/plugins/AETHtml.js
+++ plugins/AET/plugins/AETHtml.js
@@ -1,6 +1,6 @@
 /***
 |''Name''|AETHtml|
-|''Version''|0.2.4|
+|''Version''|0.2.5|
 |''Requires''|AETPlugin|
 !Usage
 Allows an easy way to copy html into a field.
@@ -19,10 +19,12 @@ config.macros.aet.controlTypes.html = function(place, tiddler, field, options) {
 	$("<input type='hidden' />").attr("edit", field).val(initial).appendTo(container);
 	var change = function(ev) {
 		var val = $(ev.target).val();
-		var value = "<html>%0</html>".format(val);
-		config.macros.aet.setMetaData(tiddler.title, field, value);
-		$(preview).empty();
-		wikify(value, preview);
+		if(val) {
+			var value = "<html>%0</html>".format(val);
+			config.macros.aet.setMetaData(tiddler.title, field, value);
+			$(preview).empty();
+			wikify(value, preview);
+		}
 	};
 	$("<textarea />").appendTo(container).change(change);
 };
