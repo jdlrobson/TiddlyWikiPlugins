a9a8ee510e09f0ef2367dc2bd7c7fc7210766ffe
Jon Robson <jdlrobson@gmail.com>
2011-01-31 09:38:30 +0000

    prevent false being saved as a field value
    Fixed bug which initialised field to value of "false"
    If empty input given the field is reset to empty string
    
    Updated meta information

--- plugins/AET/plugins/AETHtml.js
+++ plugins/AET/plugins/AETHtml.js
@@ -1,7 +1,8 @@
 /***
 |''Name''|AETHtml|
-|''Version''|0.2.5|
+|''Version''|0.2.51|
 |''Requires''|AETPlugin|
+|''Source''|https://github.com/jdlrobson/TiddlyWiki/raw/master/plugins/AET/plugins/AETHtml.js|
 !Usage
 Allows an easy way to copy html into a field.
 {{{
@@ -15,15 +16,20 @@ config.macros.aet.controlTypes.html = function(place, tiddler, field, options) {
 	var preview = $("<div />").addClass("embedPreview").appendTo(place)[0];
 	var container = $("<div />").addClass("embedArea").appendTo(place)[0];
 	var initial = config.macros.aet.getMetaData(tiddler.title, field);
-	wikify(initial, preview);
-	$("<input type='hidden' />").attr("edit", field).val(initial).appendTo(container);
+	var input = $("<input type='hidden' />").attr("edit", field).appendTo(container)[0];
+	if(initial) {
+		wikify(initial, preview);
+		$(input).val(initial);
+	}
 	var change = function(ev) {
 		var val = $(ev.target).val();
+		$(preview).empty();
 		if(val) {
 			var value = "<html>%0</html>".format(val);
 			config.macros.aet.setMetaData(tiddler.title, field, value);
-			$(preview).empty();
 			wikify(value, preview);
+		} else {
+			config.macros.aet.setMetaData(tiddler.title, field, false);
 		}
 	};
 	$("<textarea />").appendTo(container).change(change);
