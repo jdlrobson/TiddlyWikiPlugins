c399db5730822005c6ebcf681d0abad4a8e4ef51
Jon Robson <jdlrobson@gmail.com>
2011-02-02 15:54:48 +0000

    TiddlySpaceInstall meta edit
    added source to meta information

--- plugins/TiddlySpaceInstaller/TiddlySpaceInstaller.js
+++ plugins/TiddlySpaceInstaller/TiddlySpaceInstaller.js
@@ -2,6 +2,7 @@
 |''Name''|TiddlySpaceInstaller|
 |''Version''|0.3.15|
 |''Requires''|GUID|
+|''Source''|https://github.com/jdlrobson/TiddlyWiki/raw/master/plugins/TiddlySpaceInstaller/TiddlySpaceInstaller.js|
 !Usage
 {{{<<showInstall bar Foo>>}}}
 Opens the tiddler Foo to visitors of the bar space. 
