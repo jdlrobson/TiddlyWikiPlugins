8aecaf65fce4eaff6d3dc44e943ab822a171aac9
Jon Robson <jdlrobson@gmail.com>
2011-11-09 15:53:04 +0000

    update plugin meta data

--- apps/fileimport/tw_wrapper_start.js
+++ apps/fileimport/tw_wrapper_start.js
@@ -2,5 +2,7 @@
 |''Name''|TiddlyFileImportr|
 |''Version''|0.2.0|
 |''Status''|experimental|
+|''Source''|https://github.com/jdlrobson/TiddlyWikiPlugins/tree/master/apps/fileimport|
+|''Latest''|http://repository.tiddlyspace.com/TiddlyFileImportr|
 ***/
 //{{{
