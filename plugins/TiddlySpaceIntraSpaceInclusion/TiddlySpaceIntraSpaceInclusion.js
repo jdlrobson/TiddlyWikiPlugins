bfa1076099018fc4ac1e05ad9829a3cba29e02a8
Mario Pietsch <pmariojo@gmail.com>
2012-03-02 21:50:00 +0100

    story.getTiddlerText('myTiddler *(@space)*') causes the IntraSpaceInclusion regExp to detect a tiddler@space link. Using the same regExp as TiddlySpaceLinkPlugin seems to fix the bug.

--- plugins/TiddlySpaceIntraSpaceInclusion/TiddlySpaceIntraSpaceInclusion.js
+++ plugins/TiddlySpaceIntraSpaceInclusion/TiddlySpaceIntraSpaceInclusion.js
@@ -1,28 +1,38 @@
 /***
 |''Name:''|TiddlySpaceIntraSpaceInclusion|
-|''Description:''|Provides support for {{{<<tiddler Foo@bar>>}}}|
+|''Description:''|Provides support for {{{<<tiddler Foo@bar>>}}} and {{{<<tiddler [[Foo]]@bar>>}}}|
 |''Author:''|Jon Robson|
-|''Source:''|https://github.com/jdlrobson/TiddlyWikiPlugins/raw/master/plugins/TiddlySpaceIntraSpaceInclusion/TiddlySpaceIntraSpaceInclusion.js|
-|''Version:''|0.3.6|
+|''Source:''||
+|''Version:''|0.3.6MP|
 |''License:''|[[BSD License|http://www.opensource.org/licenses/bsd-license.php]] |
 |''Comments:''|Please make comments at http://groups.google.co.uk/group/TiddlyWikiDev |
 |''~CoreVersion:''|2.4|
+|''Requires''|TiddlySpaceLinkPlugin|
 ***/
 //{{{
 (function() {
 	var _tidtext = TiddlyWiki.prototype.getTiddlerText;
 	var cache = {};
+
+	// allmost the same regExp as in TiddlySpaceLinkPlugin but .. no "mg" parameter, because it didn't work for this usecase.
+	config.textPrimitives.spacenameLinkRegExp = new RegExp(config.textPrimitives.unWikiLink + 
+													 "?(" + config.textPrimitives.bareTiddlerLetter + "*)@(" + config.textPrimitives.spaceName + ")", "");
+	config.textPrimitives.tiddlyLinkSpacenameLinkRegExp = new RegExp("\\[\\[(.*?)(?:\\|(.*?))?\\]\\]@(" + config.textPrimitives.spaceName + ")", "");
+	
 	TiddlyWiki.prototype.getTiddlerText = function(title, defaultText) {
-		if(title) {
-			var match = title.match(/\[{2}([^\]]*)\]\]@(.*)/); // matches [[foo]]@bar
-			var match2 = title.match(/([^@]*)@(.*)/); // matches foo@bar
+		var ct = config.textPrimitives;
+		var match = ct.spacenameLinkRegExp.exec(title);	// foo@bar
+		var match2 = ct.tiddlyLinkSpacenameLinkRegExp.exec(title); 	// [[foo]]@bar
+		
+		if(match || match2) {
+		//	console.log('inner: ', 'spacename: ', match, 'tiddlyLink: ', match2, 'place: ');
 			var tidtitle, space;
-			if(match && match.length === 3) {
+			if(match[1] && match.length === 3) {
 				tidtitle = match[1];
 				space = match[2];
-			} else if(match2 && match2.length === 3) {
+			} else if(match2 && match2.length === 4) {
 				tidtitle = match2[1];
-				space = match2[2];
+				space = match2[3];
 			}
 			var newtitle = tidtitle + "@" + space;
 			if(tidtitle && space) {
@@ -30,6 +40,9 @@
 			}
 			if(tidtitle && space && !store.getTiddler(newtitle)) {
 				var tiddler = new Tiddler(title);
+				// get the tiddler, where the macro is rendered. //XXX will need more testing
+				var refreshTitle = story.findContainingTiddler(place).getAttribute('tiddler');;
+
 				tiddler.text = "//retrieving from server//";
 				tiddler.fields.doNotSave = "true";
 				tiddler.tags = ["excludeLists", "excludeSearch", "excludeMissing"];
@@ -42,15 +55,17 @@
 						var tiddler = store.getTiddler(title);
 						tiddler.text = tid.text;
 						store.addTiddler(tiddler);
-						store.notify(title,true);
-						story.refreshAllTiddlers(); // hacky but above link doesn't always seem to work!
+						// store.notify(title,true);
+						story.refreshTiddler(refreshTitle,null,true);
+						// story.refreshAllTiddlers(); // hacky but above link doesn't always seem to work!
 					},
 					error: function() {
 						var tiddler = store.getTiddler(title);
-						tiddler.text = "//error retrieving tiddler " + title + " from space " + space + "//";
+						tiddler.text = "//error retrieving tiddler {{{" + title + "}}} from space @" + space + "//";
 						store.addTiddler(tiddler);
-						store.notify(title,true);
-						story.refreshAllTiddlers(); // hacky but above link doesn't always seem to work!
+						// store.notify(title,true);
+						story.refreshTiddler(refreshTitle,null,true);
+						// story.refreshAllTiddlers(); // hacky but above link doesn't always seem to work!
 					}
 				});
 			}
